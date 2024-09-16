6d95e83d6991c870d80a1a11dea72bea35011396
sobiyasiva <sobiyasivakumar79@gmail.com>
2024-09-16 11:41:02 +0530

    modified tax slab

--- src/Task1/IncomeTaxCalculator.java
+++ src/Task1/IncomeTaxCalculator.java
@@ -4,23 +4,26 @@ public class IncomeTaxCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your annual salary (in lakhs): ");
-        double salary = scanner.nextDouble();
+        double salaryInLakhs = scanner.nextDouble();  
         double tax = 0;
-        if (salary > 2.5 && salary <= 5.0) {
-            tax = salary * 0.05; 
-            tax = Math.round(tax * 100.0) / 100.0; 
-            System.out.println("You have to pay " + tax + " lakhs as tax (5%).");
-        } else if (salary > 5.0 && salary <= 10.0) {
-            tax = salary * 0.10; 
-            tax = Math.round(tax * 100.0) / 100.0; 
-            System.out.println("You have to pay " + tax + " lakhs as tax (10%).");
-        } else if (salary > 10.0) {
-            tax = salary * 0.15; 
-            tax = Math.round(tax * 100.0) / 100.0; 
-            System.out.println("You have to pay " + tax + " lakhs as tax (15%).");
+        if (salaryInLakhs > 2.5 && salaryInLakhs <= 5.0) {
+            tax = salaryInLakhs * 0.05;  
+        } else if (salaryInLakhs > 5.0 && salaryInLakhs <= 10.0) {
+            tax = salaryInLakhs * 0.10;  
+        } else if (salaryInLakhs > 10.0) {
+            tax = salaryInLakhs * 0.15; 
         } else {
             System.out.println("You are not eligible to pay tax.");
+            scanner.close();
+            return; 
         }
+        tax = Math.round(tax * 100.0) / 100.0; 
+        if (tax < 1) {
+            double taxInThousands = tax * 100; 
+            System.out.println("You have to pay " + taxInThousands + " thousands as tax.");
+        } else {
+            System.out.println("You have to pay " + tax + " lakhs as tax.");
+        }       
         scanner.close();
     }
 }
